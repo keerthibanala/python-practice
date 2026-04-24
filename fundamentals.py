6da7586bfb89983431203ed5050ac2ce3d0b9bd6
Keerthi Banala <keerthipriya.banala@gmail.com>
2026-04-24 16:46:12 -0500

    Python fundamentals practice

--- fundamentals.py
+++ fundamentals.py
@@ -1,1 +1,42 @@
+# This is a python file that contains the fundamentals of python programming language
+# printing
 print("Hello, World!")
+print()
+print("My name","is", "Keerthi Banala")
+
+# variables
+print("--------variables----")
+name = "Keerthi Banala"
+age = 20
+is_learning = True
+print("Name:", name)
+print("Age:", age)
+print("Is Learning:", is_learning)
+
+# data types
+print("--------data types----")
+print("Data type of name:", type(name))
+print("Data type of age:", type(age))
+print("Data type of is_learning:", type(is_learning))
+
+# basic operations
+print("--------basic operations----")
+a = 10
+b = 5
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Integer Division:", a // b)
+print("Modulus:", a % b)
+print("Exponentiation:", a ** b)    
+
+# converting data types
+print("--------converting data types----")
+num_str = "100"
+num_int = int(num_str)
+print("String to Integer:", num_int)
+num_float = float(num_str)
+print("String to Float:", num_float)
+num_str_back = str(num_int)
+print("Integer to String:", num_str_back)   
